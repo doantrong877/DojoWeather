4ee5e33e0434747639e0ade17df46e2c287e7b3e
Trong Doan <doantrong8777@gmail.com>
2022-06-22 14:26:01 -0700

    change to js file to convert temperature

--- script.js
+++ script.js
@@ -6,6 +6,46 @@ cities.forEach(city => {
     city.addEventListener('click', function handleClick(event){
         alert("Loading weather report of " + city.innerHTML);
     })
-})
+});
+
+//Make Cookie disapear
+
+var acceptBnt = document.querySelector("#accept-btn");
+
+acceptBnt.addEventListener("click", function(){
+    document.querySelector(".cookie").remove();
+});
+
+//change temperature
+
+var tempOption = document.querySelector("#temp-select");
+
+tempOption.addEventListener("change",function(){
+    //change to fehranheit
+    if(tempOption.value == "fehranheit"){
+        var hi = document.querySelectorAll("#high h3");
+        var lo = document.querySelectorAll("#low h3");
+        hi.forEach(high => {
+            high.innerHTML = Math.floor(Number(high.innerHTML) * 1.8 + 32)         
+        });
+        lo.forEach(low => {
+            low.innerHTML = Math.floor(Number(low.innerHTML) * 1.8 + 32)         
+        });   
+    }
+    //change to celsius
+    if(tempOption.value == "celcius"){
+        var hi = document.querySelectorAll("#high h3");
+        var lo = document.querySelectorAll("#low h3");
+        hi.forEach(high => {
+            high.innerHTML = Math.ceil((Number(high.innerHTML) - 32) / 1.8)         
+        });
+        lo.forEach(low => {
+            low.innerHTML = Math.ceil((Number(low.innerHTML) - 32) / 1.8)         
+        });   
+    }
+});
+
+
+
 
 
